d6267b618d3e38f171d02ab705137f49a8d04ee9
muhammadadnanoukal <101559768+muhammadadnanoukal@users.noreply.github.com>
2023-04-05 15:46:34 +0300

    Delete ALTANYMYA_NASR_PACKAGING directory

--- ALTANYMYA_NASR_PACKAGING/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import mrp_production
-from . import mrp_bom
-from . import product_template
-from . import sale_order_line
--- ALTANYMYA_NASR_PACKAGING/models/sale_order_line.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models
-
-
-class SaleOrderLineNasr(models.Model):
-    _inherit = 'sale.order.line'
-    job_type = fields.Selection([("repeat", "Repeat"), ("revised", "Revised"), ("new", "New")], default='new')
